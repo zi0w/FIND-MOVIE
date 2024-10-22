7247d7354c30a9aecf3513747b5dfe352e5de274
zi0w <zzz13588@naver.com>
2024-10-22 14:39:28 +0900

    day3: 검색 기능 구현

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const options = {
   },
 };
 
+let movies = [];
+
 fetch(
   //url 변경
   "https://api.themoviedb.org/3/movie/top_rated?language=ko-KR&page=1",
@@ -14,28 +16,53 @@ fetch(
 )
   .then((response) => response.json())
   .then((response) => {
-    console.log(response.results);
-
-    response.results.forEach((element) => {
-      const tempHtml = `
-      <div class="card-box">
-        <div class="card-image-box">
-          <img class="movie-image" 
-          src="https://image.tmdb.org/t/p/w500${element.poster_path}" 
-          alt="${element.title} 포스터 이미지"/>
-        </div>
-        <div class="card-text-box">
-          <h4 class="movie-title">${element.title}</h4>
-          <p class="movie-rating">평점: ${element.vote_average}</p>
-        </div>
-      </div> 
-      `;
-      document.querySelector(".movie-container").innerHTML += tempHtml;
-    });
+    movies = response.results;
+    displayMovies(movies);
   })
-  // 에러 처리 -> 에러 발생시 콘솔에 에러 메세지 출력
-  // 사용자 입장에서의 에러 처리가 필요하다(alert!)
   .catch((err) => {
     console.error("에러 발생: ", err);
     alert("에러가 발생했습니다. 다시 시도해 주세요.");
   });
+
+function displayMovies(movieList) {
+  const movieContainer = document.querySelector(".movie-container");
+  movieContainer.innerHTML = "";
+
+  movieList.forEach((element) => {
+    const tempHtml = `
+    <div class="card-box">
+      <div class="card-image-box">
+        <img class="movie-image" 
+        src="https://image.tmdb.org/t/p/w500${element.poster_path}" 
+        alt="${element.title} 포스터 이미지"/>
+      </div>
+      <div class="card-text-box">
+        <h4 class="movie-title">${element.title}</h4>
+        <p class="movie-rating">평점: ${element.vote_average}</p>
+      </div>
+    </div> 
+    `;
+    movieContainer.innerHTML += tempHtml;
+  });
+}
+
+// 검색 필터링 함수(대소문자, 띄어쓰기 무시)
+function filterMovies(searchInput) {
+  const searchKeywords = searchInput.toLowerCase().replace(/\s/g, "");
+  const filterMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().replace(/\s/g, "").includes(searchKeywords)
+  );
+  displayMovies(filterMovies);
+}
+
+// 실시간 검색(디바운싱)
+let debounceTimer;
+
+document.querySelector(".header-input").addEventListener("input", function () {
+  clearTimeout(debounceTimer);
+
+  debounceTimer = setTimeout(() => {
+    const searchInput = document.querySelector(".header-input").value;
+    filterMovies(searchInput);
+  }, 500);
+});
